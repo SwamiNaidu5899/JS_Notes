7e1cee7be71120bd4ecca798abdf8ab00651b274
Swami Naidu Datti <152952658+SwamiNaidu5899@users.noreply.github.com>
2024-01-29 16:02:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function topic1()
+{
+    document.getElementById("topic-2").style.display = 'none';
+    document.getElementById("topic-2").style.display = 'none';
+    document.getElementById("topic-1").style.display = 'inline';
+}
+function topic2()
+{
+    document.getElementById("topic-1").style.display = 'none';
+    document.getElementById("topic-2").style.display = 'inline';
+    document.getElementById("topic-3").style.display = 'none';
+}
+function topic3()
+{
+    document.getElementById("topic-1").style.display = 'none';
+    document.getElementById("topic-2").style.display = 'none';
+    document.getElementById("topic-3").style.display = 'inline';
+}
